e5086ad70ddbbd99a4b61978d014e2fdea3bd717
Esteban TEMPLIER <esteban.templier82@gmail.com>
2023-03-17 03:49:08 +0100

    création de la modal et ajout des médias à la gallerie

--- FrontEnd/js/projets.js
+++ FrontEnd/js/projets.js
@@ -6,6 +6,15 @@ function affichages(tous) {
   affichage += "</div>";
   document.querySelector(".gallery").innerHTML = affichage;
 }
+function affichagesGalleriePhoto(tous) {
+  let affichage = "<div>";
+  for (let projet of tous) {
+    affichage += `<figure class="edition">    <img src="${projet.imageUrl}" alt="${projet.title}"></img>  <i class="fa-sharp fa-solid fa-arrows-up-down-left-right"></i>  <label for="${projet.id}"> <input type="checkbox" name="delete" id="${projet.id}">   <span class="edit"><i class="fa-sharp fa-solid fa-trash-can"></i></span>   </label>  <figcaption> éditer </figcaption>    </figure>`;
+  }
+  affichage += "</div>";
+  document.querySelector("#galeriePhoto").innerHTML = affichage;
+}
+
 
 fetch("http://localhost:5678/api/works")
 .then(function(res) {
@@ -14,30 +23,30 @@ fetch("http://localhost:5678/api/works")
   }
 })
 .then(function (value) {
-  const Tous1 = value.filter(obj => obj.categoryId > 0);
-  console.log(Tous1);
-  const Objets1 = value.filter(obj => obj.categoryId == 1);
-  console.log(Objets1);
-  const appartements1 = value.filter(obj => obj.categoryId == 2);
-  console.log(appartements1);
-  const hotelsRestaurants1 = value.filter(obj => obj.categoryId == 3);
-  console.log(hotelsRestaurants1);
+  const tous = value.filter(obj => obj.categoryId > 0);
+  console.log(tous);
+  const objets = value.filter(obj => obj.categoryId == 1);
+  console.log(objets);
+  const appartements = value.filter(obj => obj.categoryId == 2);
+  console.log(appartements);
+  const hotelsRestaurants = value.filter(obj => obj.categoryId == 3);
+  console.log(hotelsRestaurants);
   
   let elts = document.querySelectorAll("input[name='filtre']");
   
   let findSelected = () => {
     let selected = document.querySelector("input[name='filtre']:checked").value;
     if (selected == 0) {
-      affichages(Tous1);
+      affichages(tous);
     }
     else if (selected == 1) {
-      affichages(Objets1);
+      affichages(objets);
     }
     else if (selected == 2) {
-      affichages(appartements1);
+      affichages(appartements);
     }
     else if (selected == 3) {
-      affichages(hotelsRestaurants1);
+      affichages(hotelsRestaurants);
     }
   }
 
@@ -63,38 +72,84 @@ if (token) {
     location.reload();
   });
 
+  document.querySelector("#publication").style.display = "flex";
+  document.querySelector("header").style.margin = "40px auto 50px auto";
   document.querySelector(".filtres").style.display = "none";
   document.querySelector("#portfolio h2").style.margin = "2em 0";
   document.querySelector("#fig a").style.display = "block";
   document.querySelector(".containerModale a").style.display = "block";
+
+  let modal = document.querySelector("#modal1");
+  const focusableSelector = 'button, a';
+  let focusable = [];
+  let previouslyFocusedElement = null;
+
+  document.querySelector(".modalLink").addEventListener("click", function() {
+    focusable = Array.from(modal.querySelectorAll(focusableSelector));
+    previouslyFocusedElement = document.querySelector(':focus');
+    modal.style.display = null;
+    focusable[0].focus();
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    modal.addEventListener('click', closeModal);
+    document.querySelector(".jsModalClose").addEventListener("click", closeModal);
+    document.querySelector(".jsModalStop").addEventListener("click", stopPropagation);
+  })
+
+  const closeModal = function (e) {
+    e.preventDefault();
+    if (modal.style.display = "none") return
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    document.querySelector(".jsModalClose").removeEventListener("click", closeModal);
+    document.querySelector(".jsModalStop").removeEventListener("click", stopPropagation);
+  }
+  
+  const stopPropagation = function(e) {
+    e.stopPropagation();
+  }
+  const focusInModal = function (e) {
+    e.preventDefault();
+    let index = focusable.findIndex(f => f === modal.querySelector(':focus'));
+    if (e.shiftKey === true) {
+      index--;
+    }else {
+      index++;
+    }
+    if (index >= focusable.length) {
+      index = 0;
+    }
+    if (index < 0) {
+      index = focusable.length -1;
+    }
+    focusable[index].focus();
+  }
+
+  window.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" || e.key ==="Esc") {
+      closeModal(e);
+    }
+    else if (e.key === 'Tab' && modal !== null) {
+      focusInModal(e);
+    }
+  })
+  fetch("http://localhost:5678/api/works")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (value) {
+    const tous = value.filter(obj => obj.categoryId > 0);
+    console.log(tous);
+    document.querySelector(".modalLink").addEventListener("click", affichagesGalleriePhoto(tous));
+
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+  
 }
-
-// let button = form.submit.addEventListener("click", (e)=> {
-//   e.preventDefault();
-//   const login = 'http://localhost:5678/api/users/login';
-
-//   fetch(login, {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json, text/plain, */*",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       email: form.email.value,
-//       password: form.password.value,
-//     }),
-//   })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data)
-//     localStorage.setItem('tokens', JSON.stringify(data));
-//     if (data.error) {
-//       alert("!! ERROR !! 'Email' or 'Password' --- incorrect ---");
-//     }else {
-//       window.location.assign("../index.html");
-//     }
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// });
