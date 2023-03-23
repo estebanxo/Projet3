df3e4eff987b276fb88e435b60be11e15da88d4a
Esteban TEMPLIER <esteban.templier82@gmail.com>
2023-03-23 16:51:51 +0100

    avancement à 70% (modal avec ajout d'image et suppression)

--- FrontEnd/js/projets.js
+++ FrontEnd/js/projets.js
@@ -7,9 +7,9 @@ function affichages(tous) {
   document.querySelector(".gallery").innerHTML = affichage;
 }
 function affichagesGalleriePhoto(tous) {
-  let affichage = "<div>";
+  let affichage = '<div id="imgWorks">';
   for (let projet of tous) {
-    affichage += `<figure class="edition">    <img src="${projet.imageUrl}" alt="${projet.title}"></img>  <i class="fa-sharp fa-solid fa-arrows-up-down-left-right"></i>  <label for="${projet.id}"> <input type="checkbox" name="delete" id="${projet.id}">   <span class="edit"><i class="fa-sharp fa-solid fa-trash-can"></i></span>   </label>  <figcaption> éditer </figcaption>    </figure>`;
+    affichage += `<figure class="edition">    <img src="${projet.imageUrl}" alt="${projet.title}"></img>  <i class="fa-sharp fa-solid fa-arrows-up-down-left-right"></i>  <label for="${projet.id}"> <input type="checkbox" name="delete" id="${projet.id}" value=${projet.id}>   <span class="edit"><i class="fa-sharp fa-solid fa-trash-can"></i></span>   </label>  <figcaption> éditer </figcaption>    </figure>`;
   }
   affichage += "</div>";
   document.querySelector("#galeriePhoto").innerHTML = affichage;
@@ -25,6 +25,9 @@ fetch("http://localhost:5678/api/works")
 .then(function (value) {
   const tous = value.filter(obj => obj.categoryId > 0);
   console.log(tous);
+  const last = tous[tous.length-1];
+  console.log(last.id);
+
   const objets = value.filter(obj => obj.categoryId == 1);
   console.log(objets);
   const appartements = value.filter(obj => obj.categoryId == 2);
@@ -60,8 +63,13 @@ fetch("http://localhost:5678/api/works")
   // Une erreur est survenue
 });
 
-const token = localStorage.getItem('tokens');
-console.log(token);
+// for (var i = 0; i < localStorage.length; i++) {
+//   console.log(localStorage.getItem(localStorage.key(i)));
+// }
+
+console.log(localStorage.getItem('tokens'));
+const token = JSON.parse(localStorage.getItem('tokens'));
+console.log(token.token);
 
 if (token) {
   document.querySelector('#in').style.display = "none";
@@ -136,6 +144,7 @@ if (token) {
       focusInModal(e);
     }
   })
+
   fetch("http://localhost:5678/api/works")
   .then(function(res) {
     if (res.ok) {
@@ -146,13 +155,159 @@ if (token) {
     const tous = value.filter(obj => obj.categoryId > 0);
     console.log(tous);
     document.querySelector(".modalLink").addEventListener("click", affichagesGalleriePhoto(tous));
-
   })
   .catch(function(err) {
     // Une erreur est survenue
   });
+
   
-  document.querySelector(".buttonPhoto").addEventListener("click", function () {
+  document.querySelector(".supprimer").addEventListener("click", function () {
+
+    let valueToDelete = document.querySelectorAll("input[name='delete']");
+
+    let deletion = () => {
+      let id = document.querySelector("input[name='delete']:checked").value;
+      fetch(`http://localhost:5678/api/works/${id}`, {
+        method: "DELETE",
+        body: null,
+        headers: {
+          "Content-type": "application/json; charsert=UTF-8",
+        },
+      })
+      .then((reponse) => reponse.json())
+
+      .then((json) => alert("Gallerie supprimé"))
+
+      .catch(function(err) {
+        // Une erreur est survenue
+      });
+    }
+
+    valueToDelete.forEach(valueToDelete => {
+      valueToDelete.addEventListener("checked", deletion);
+    })
+  })
+
+  document.querySelector(".buttonPhoto").addEventListener("click", function() {
+    document.querySelector("#jsModalReturn").style.display = "var(--fa-display,inline-block)";
+    document.querySelector("#titleModal").style.display = "none";
+    document.querySelector("#titleModal2").style.display = "block";
+    document.querySelector("#galeriePhoto div").style.display = "none";
+    document.querySelector(".buttonPhoto").style.display = "none";
+    document.querySelector(".valider").style.display = "inline-block";
+    document.querySelector(".supprimer").style.display = "none";
+    document.querySelector("#formAjoutPhoto").style.display = "flex";
+    let affichage = "<div>";
+
+    affichage += '<label class="containerPhoto" for="photo">';
+    affichage += '<span id="img"> <i class="fa-regular fa-image"></i> </span>';
+    affichage += '<span class="buttonAjoutPhoto"> + Ajouter photo </span>';
+    affichage += '<input type="file" id="photo" name="photo" accept="image/*" onchange="afficheImage();">'
+    affichage += '<span id="imgNotification">jpg, png : 4mo max</span>';
+    affichage += '</label>';
+
+    affichage += '<label class="weight" for="title">Titre</label>';
+    affichage += '<input type="text" name="title" id="title">';
+
+    affichage += '<label class="weight" for="categorie">Catégorie</label>';
+    affichage += '<select name="categorie" id="categorie">';
+    affichage += '<option name="option" value="1">Objets</option>';
+    affichage += '<option name="option" value="2">Appartements</option>';
+    affichage += '<option name="option" value="3" selected>Hotels & Restaurants</option>';
+    affichage += '</select>';
+
+    affichage += "</div>";
+    document.querySelector("#galeriePhoto").innerHTML = affichage;
 
   })
+
+  function afficheImage() {
+    let file = document.querySelector('input[type="file"]').files;
+    console.log(file);
+    if(file.length = 1) {
+      document.querySelector("#img").style.display = "none";
+      document.querySelector(".buttonAjoutPhoto").style.display = "none";
+      document.querySelector("#photo").style.display = "none";
+      document.querySelector("#imgNotification").style.display = "none";
+      document.querySelector(".containerPhoto").style.padding = "0";
+      let newAffichage = '<img id="resultat">';
+      document.querySelector(".containerPhoto").innerHTML = newAffichage;
+
+      let fileReader = new FileReader();
+      fileReader.onload = function (event) {
+        document
+          .getElementById("resultat")
+          .setAttribute('src', event.target.result);
+      }
+      fileReader.readAsDataURL(file[0]);
+    }
+  }
+
+  fetch("http://localhost:5678/api/categories")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((value) => {
+
+    const objets = value.filter(obj => obj.id == 1);
+    console.log(objets);
+
+    const appartements = value.filter(obj => obj.id == 2);
+    console.log(appartements);
+
+    const hotelsRestaurants = value.filter(obj => obj.id == 3);
+    console.log(hotelsRestaurants);
+
+    let findSelectElts = () => {
+      let selectedOption = document.querySelector("option[name='option']:selected").value;
+      if (selectedOption == 1) {
+        return objets;
+      }
+      else if (selectedOption == 2) {
+        return appartements;
+      }
+      else if (selectedOption == 3) {
+        return hotelsRestaurants;
+      }
+    }
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+
+  document.querySelector(".valider").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let photo = document.querySelector("#photo").value;
+    console.log(photo);
+    let selectedOption = document.querySelector("option[name='option']:selected").value;
+    if(photo === "") {
+      alert("Erreur: image Obligatoire");
+      return; 
+    }
+    if(title.trim() === "") {
+      alert("Erreur: Titre Obligatoire");
+      return;
+    }
+    else {
+      fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        body: JSON.stringify({
+          id: last.id,
+          title: title,
+          imageUrl: photo,
+          categoryId: selectedOption,
+          userId: token[10],
+        }),
+        headers: {
+          "Content-type": "application/json; charsert=UTF-8",
+        },
+      })
+      .then((res) => res.json()) 
+      .then((json) => console.log(json));
+    }
+  })
 }
+
